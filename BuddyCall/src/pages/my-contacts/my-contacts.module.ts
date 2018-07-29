dfc60d9db69c5fe0d656eb5834edf994a5a3968d
Kevin Kauffman <dokkevin@gmail.com>
2018-07-29 15:12:34 -0800

    Display an unrelated list
    I'm displaying a list, but the info in it is unrelated. I just wanted to learn how to do a list. I can't access the contacts as as soon as I try to get contacts it says the plugin isn't installed. This may be because the contacts plugin is deprecated and requires an older version of something in npm that I need to reinstall.

--- BuddyCall/src/pages/my-contacts/my-contacts.module.ts
+++ BuddyCall/src/pages/my-contacts/my-contacts.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MyContactsPage } from './my-contacts';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 
 @NgModule({
   declarations: [
--- BuddyCall/src/pages/my-contacts/my-contacts.ts
+++ BuddyCall/src/pages/my-contacts/my-contacts.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the MyContactsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 
 @IonicPage()
 @Component({
   selector: 'page-my-contacts',
   templateUrl: 'my-contacts.html',
+  providers: [Contacts],
 })
 export class MyContactsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-      // Installed Contacts Thingy, now gather and display them.
+    public allContacts: any
+    public heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private contacts: Contacts) {
+        // This gives an error -> it says the plugin isn't installed. Seems like it may be 
+        // this.contacts.find(['displayName', 'name', 'phoneNumbers', 'emails'], {filter: "", multiple: true})
+        // .then(data => {
+        //     this.allContacts = data
+        // });
   }
 
   ionViewDidLoad() {
