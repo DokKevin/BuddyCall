889c93bd79b0b3e8b61afaffed84c2fb4e5542dd
Kevin Kauffman <dokkevin@gmail.com>
2018-08-04 14:08:58 -0800

    Can now access contacts
    Only works when deploying to my phone, can't use serve for the browser or the DevApp. Has a couple of issues recorded as TODO's in the code.

--- BuddyCall/src/pages/home/home.ts
+++ BuddyCall/src/pages/home/home.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MyContactsPage } from '../my-contacts/my-contacts';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [Contacts],
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public allContacts: any
+
+  constructor(public navCtrl: NavController, private contacts: Contacts, public platform: Platform) {
 
   }
 
   navToGetContacts(){
     // TODO: This lets me hit back button to gather contacts again. Do I want this?
-    this.navCtrl.push(MyContactsPage);
+    this.platform.ready().then(() =>{
+        // TODO: Asks me if I will allow it to access my contacts and then I have to push button again. 
+        this.contacts.find(['displayName', 'name', 'phoneNumbers', 'emails'], {filter: "", multiple: true})
+        .then(data => {
+            this.allContacts = data
+        });
+    });
+
+    
+
+    // this.allContacts = ['Test', 'From', 'Home'];
+
+    this.navCtrl.push(MyContactsPage, {contacts: this.allContacts});
   }
 
 }
--- BuddyCall/src/pages/my-contacts/my-contacts.ts
+++ BuddyCall/src/pages/my-contacts/my-contacts.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+import { Platform } from 'ionic-angular';
 
 @IonicPage()
 @Component({
   selector: 'page-my-contacts',
   templateUrl: 'my-contacts.html',
-  providers: [Contacts],
 })
 export class MyContactsPage {
 
     public allContacts: any
-    public heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private contacts: Contacts) {
-        // This gives an error -> it says the plugin isn't installed. Seems like it may be 
-        // this.contacts.find(['displayName', 'name', 'phoneNumbers', 'emails'], {filter: "", multiple: true})
-        // .then(data => {
-        //     this.allContacts = data
-        // });
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.allContacts = navParams.get('contacts');
   }
 
   ionViewDidLoad() {
